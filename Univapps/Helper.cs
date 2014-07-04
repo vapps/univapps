5d57ff344fed0429c1c59d38c45b5d1bb25ba734
Reagan Hwang <rghwang@live.com>
2014-07-04 17:58:29 +0900

    Univapp로 이름 변경

--- Univapps/Helper.cs
+++ Univapps/Helper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Windows.ApplicationModel.Background;
 
-namespace Vapps
+namespace Univapps
 {
     public sealed class Helper
     {
--- Univapps/Properties/AssemblyInfo.cs
+++ Univapps/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Helper")]
+[assembly: AssemblyTitle("Univapps")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Helper")]
+[assembly: AssemblyProduct("Univapps")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
