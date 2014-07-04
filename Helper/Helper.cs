c04c156a60377a4d1110fe7ca8d1ce246d326eb7
Reagan Hwang <rghwang@live.com>
2014-07-04 11:46:16 +0900

    initial commit

---
+++ Helper/Helper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.ApplicationModel.Background;
+
+namespace Vapps
+{
+    public sealed class Helper
+    {
+        // RegisterBackgroundTask()
+        // referenece : http://msdn.microsoft.com/en-us/library/windows/apps/xaml/jj553413.aspx
+        public static BackgroundTaskRegistration RegisterBackgroundTask(
+                                                string taskEntryPoint,
+                                                string name,
+                                                IBackgroundTrigger trigger,
+                                                IBackgroundCondition condition)
+        {
+            //
+            // Check for existing registrations of this background task.
+            //
+            //
+
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+            {
+
+                if (cur.Value.Name == name)
+                {
+                    // 
+                    // The task is already registered.
+                    // 
+
+
+                    return (BackgroundTaskRegistration)(cur.Value);
+                }
+            }
+
+            // Register the background task.
+
+            var builder = new BackgroundTaskBuilder();
+
+            builder.Name = name;
+            builder.TaskEntryPoint = taskEntryPoint;
+            builder.SetTrigger(trigger);
+
+            if (condition != null)
+            {
+                builder.AddCondition(condition);
+            }
+
+            BackgroundTaskRegistration task = builder.Register();
+
+            return task;
+
+        }
+    }
+}
